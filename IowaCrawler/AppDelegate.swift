ab1d8aa5bf849c1995ba7cf54c0b6d81aabdf25d
Seungwon Ju <dshseungwon@gmail.com>
2020-08-05 14:21:09 +0900

    Initial Commit

---
+++ IowaCrawler/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  IowaCrawler
+//
+//  Created by Seungwon Ju on 2020/08/05.
+//  Copyright © 2020 Seungwon Ju. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ IowaCrawler/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  IowaCrawler
+//
+//  Created by Seungwon Ju on 2020/08/05.
+//  Copyright © 2020 Seungwon Ju. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
